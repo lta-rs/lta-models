b54305a5aa9d86ce7d7d96d833fe3fe503dc4e54
BudiNverse <me@inve.rs>
2021-01-10 20:16:11 +0800

    - Added macro_use for lib.rs
    - Ported over utils from lta_utils_common to utils.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,15 @@
 //! Data Structures for lta-rs
 
+#[macro_use]
+extern crate lazy_static;
+
 pub mod bus;
 pub mod bus_enums;
 pub mod crowd;
 pub mod taxi;
 pub mod traffic;
 pub mod train;
+pub mod utils;
 
 /// Data structures for all data
 pub mod prelude {
