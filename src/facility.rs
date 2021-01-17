47c6c858fce69442c57915c52052fa5f9ec88b54
BudiNverse <me@inve.rs>
2021-01-18 00:21:11 +0800

    Moved facilities structs to facilities_maintenance mod

--- src/facility.rs
+++ src/facility.rs
@@ -1,56 +1,65 @@
 use chrono::{DateTime, FixedOffset};
 
-#[derive(Debug, Clone, PartialEq, Deserialize, Serialize)]
-pub struct FacilityMaintenanceRawResp {
-    pub value: Vec<FacilityLink>
+pub mod prelude {
+    pub use crate::facility::facilities_maintenance::*;
 }
 
-#[derive(Debug, Clone, PartialEq, Deserialize, Serialize)]
-#[serde(rename_all(deserialize = "PascalCase"))]
-pub struct FacilityLink {
-    pub link: String,
-    pub timestamp: DateTime<FixedOffset>
-}
 
-#[derive(Debug, Clone, PartialEq, Deserialize, Serialize)]
-pub enum FacilityId {
-    ArrowMarking,
-    Bollard,
-    BusStopLocation,
-    ControlBox,
-    ConvexMirror,
-    CoveredLinkWay,
-    CyclingPath,
-    CyclingPathConstruction,
-    DetectorLoop,
-    EmergencyGate,
-    ERPGantry,
-    Footpath,
-    GuardRail,
-    KerbLine,
-    LampPost,
-    LaneMarking,
-    ParkingStandardsZone,
-    PassengerPickupBay,
-    #[serde(alias = "PedestrainOverheadbridge_UnderPass")]
-    PedestrianOverheadBridgeOrUnderPass,
-    RailConstruction,
-    Railing,
-    RetainingWall,
-    RoadConstruction,
-    RoadCrossing,
-    RoadHump,
-    RoadSectionLine,
-    SchoolZone,
-    SilverZone,
-    SpeedRegulatingStrip,
-    StreetPaint,
-    TaxiStand,
-    TrafficLight,
-    TrafficSign,
-    TrainStation,
-    TrainStationExit,
-    #[serde(alias = "VehicularBridge_Flyover_Underpass")]
-    VehicularBridgeOrFlyoverOrUnderpass,
-    WordMarking
+pub mod facilities_maintenance {
+    use chrono::{DateTime, FixedOffset};
+
+    #[derive(Debug, Clone, PartialEq, Deserialize, Serialize)]
+    pub struct FacilityMaintenanceRawResp {
+        pub value: Vec<FacilityLink>
+    }
+
+    #[derive(Debug, Clone, PartialEq, Deserialize, Serialize)]
+    #[serde(rename_all(deserialize = "PascalCase"))]
+    pub struct FacilityLink {
+        pub link: String,
+        pub timestamp: DateTime<FixedOffset>
+    }
+
+    #[derive(Debug, Clone, PartialEq, Deserialize, Serialize)]
+    pub enum FacilityId {
+        ArrowMarking,
+        Bollard,
+        BusStopLocation,
+        ControlBox,
+        ConvexMirror,
+        CoveredLinkWay,
+        CyclingPath,
+        CyclingPathConstruction,
+        DetectorLoop,
+        EmergencyGate,
+        ERPGantry,
+        Footpath,
+        GuardRail,
+        KerbLine,
+        LampPost,
+        LaneMarking,
+        ParkingStandardsZone,
+        PassengerPickupBay,
+        #[serde(alias = "PedestrainOverheadbridge_UnderPass")]
+        PedestrianOverheadBridgeOrUnderPass,
+        RailConstruction,
+        Railing,
+        RetainingWall,
+        RoadConstruction,
+        RoadCrossing,
+        RoadHump,
+        RoadSectionLine,
+        SchoolZone,
+        SilverZone,
+        SpeedRegulatingStrip,
+        StreetPaint,
+        TaxiStand,
+        TrafficLight,
+        TrafficSign,
+        TrainStation,
+        TrainStationExit,
+        #[serde(alias = "VehicularBridge_Flyover_Underpass")]
+        VehicularBridgeOrFlyoverOrUnderpass,
+        WordMarking
+    }
 }
