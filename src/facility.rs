cc35a351195a18cbcee10fa5c2305788eb3a9dee
BudiNverse <me@inve.rs>
2021-01-18 00:27:40 +0800

    Added geospatial structs
    Moved FacilityId to GeospatialId. Wrong impl for facility

--- src/facility.rs
+++ src/facility.rs
@@ -18,47 +18,4 @@ pub mod facilities_maintenance {
         pub link: String,
         pub timestamp: DateTime<FixedOffset>,
     }
-
-    #[derive(Debug, Clone, PartialEq, Deserialize, Serialize)]
-    pub enum FacilityId {
-        ArrowMarking,
-        Bollard,
-        BusStopLocation,
-        ControlBox,
-        ConvexMirror,
-        CoveredLinkWay,
-        CyclingPath,
-        CyclingPathConstruction,
-        DetectorLoop,
-        EmergencyGate,
-        ERPGantry,
-        Footpath,
-        GuardRail,
-        KerbLine,
-        LampPost,
-        LaneMarking,
-        ParkingStandardsZone,
-        PassengerPickupBay,
-        #[serde(alias = "PedestrainOverheadbridge_UnderPass")]
-        PedestrianOverheadBridgeOrUnderPass,
-        RailConstruction,
-        Railing,
-        RetainingWall,
-        RoadConstruction,
-        RoadCrossing,
-        RoadHump,
-        RoadSectionLine,
-        SchoolZone,
-        SilverZone,
-        SpeedRegulatingStrip,
-        StreetPaint,
-        TaxiStand,
-        TrafficLight,
-        TrafficSign,
-        TrainStation,
-        TrainStationExit,
-        #[serde(alias = "VehicularBridge_Flyover_Underpass")]
-        VehicularBridgeOrFlyoverOrUnderpass,
-        WordMarking,
-    }
 }
--- src/geo.rs
+++ src/geo.rs
@@ -1,1 +1,62 @@
 
+pub mod prelude {
+
+}
+
+pub mod geospatial_whole_island {
+    use chrono::{DateTime, FixedOffset};
+
+    #[derive(Debug, Clone, PartialEq, Deserialize, Serialize)]
+    pub struct GeospatialWholeIslandRawResp {
+        pub value: Vec<GeospatialLink>,
+    }
+
+    #[derive(Debug, Clone, PartialEq, Deserialize, Serialize)]
+    #[serde(rename_all(deserialize = "PascalCase"))]
+    pub struct GeospatialLink {
+        pub link: String
+    }
+
+    #[derive(Debug, Clone, PartialEq, Deserialize, Serialize)]
+    pub enum GeospatialLayerId {
+        ArrowMarking,
+        Bollard,
+        BusStopLocation,
+        ControlBox,
+        ConvexMirror,
+        CoveredLinkWay,
+        CyclingPath,
+        CyclingPathConstruction,
+        DetectorLoop,
+        EmergencyGate,
+        ERPGantry,
+        Footpath,
+        GuardRail,
+        KerbLine,
+        LampPost,
+        LaneMarking,
+        ParkingStandardsZone,
+        PassengerPickupBay,
+        #[serde(alias = "PedestrainOverheadbridge_UnderPass")]
+        PedestrianOverheadBridgeOrUnderPass,
+        RailConstruction,
+        Railing,
+        RetainingWall,
+        RoadConstruction,
+        RoadCrossing,
+        RoadHump,
+        RoadSectionLine,
+        SchoolZone,
+        SilverZone,
+        SpeedRegulatingStrip,
+        StreetPaint,
+        TaxiStand,
+        TrafficLight,
+        TrafficSign,
+        TrainStation,
+        TrainStationExit,
+        #[serde(alias = "VehicularBridge_Flyover_Underpass")]
+        VehicularBridgeOrFlyoverOrUnderpass,
+        WordMarking,
+    }
+}
